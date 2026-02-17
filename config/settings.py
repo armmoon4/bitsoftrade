7a36f473ac99f66508ee20ec1f0ae2324e42feb7
armmoon4 <rahman15-4343@diu.edu.bd>
2026-02-17 16:44:46 +0600

    access token time extended

--- config/settings.py
+++ config/settings.py
@@ -169,8 +169,8 @@ REST_FRAMEWORK = {
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
